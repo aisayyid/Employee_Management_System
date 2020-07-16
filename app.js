938881ca445ea8f924926062dbc7d5b65f0ce01c
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-15 22:13:30 -0400

    Added queries to add employee to database.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var mysql = require("mysql");
+var mysql = require("mysql2/promise");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
+var connection;
 
+async function startup() {
 var PORT = process.env.PORT || 3306;
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+connection = await mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -19,15 +22,18 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   // run the start function after the connection is made to prompt the user
+  addEmployee();
 
-});
+// });
 console.log("Server is listening on port " + PORT);
+}
 
-connection.end();
+startup();
+//use later
+// connection.end();
 
 //Set up inquirer prompts.
 //First prompt should have options to add an employee, view an employee, or update employee role.
@@ -37,9 +43,70 @@ function start() {
       name: "startOptions",
       type: "list",
       message: "Would you like to add, update, or view employees?",
-      choices: ["Add employee", "Update Employee", "View employees", "Exit"]
+      choices: ["Add employee", "Update employee", "View employees", "Exit"]
     })
-  }
+}
 //If add employee, should ask you first name, then last name, then list of roles, then id.
 //If update employee roles, list of roles to replace current role.
-//If view employees is chose, should show employees witht heir department, and roles.
+//If view employees is chosen, should show employees with their department, and roles.
+
+//function that will run questions about Employees.
+async function addEmployee() {
+  //get roles from db for inquirer
+  let [roles, data] = await connection.query("SELECT id, title from employeesdb.roles")
+  roles = roles.map(role => {return { name: role.title, value: role.id }})
+  // console.log(roles);
+
+  
+  const answers = await inquirer
+    .prompt([
+      {
+        name: "first_name",
+        type: "input",
+        message: "What is your employee's first name?"
+
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is your employee's last name?"
+      }
+      ,{
+        type: "list",
+        name: "role_id",
+        message: "What is your employee's role?",
+        choices: roles
+      }
+    ])
+  console.log(answers);
+  await connection.query("INSERT INTO employee SET ?", answers);
+
+    viewEmployees();
+  
+}
+
+function startChoices() {
+  inquirer.prompt()
+    .then(function (answer) {
+      ///determine which choice was selected
+      switch (answer.startOptions) {
+        case "Add employee":
+
+          break;
+        case "Update employee":
+
+          break;
+        case "View employees":
+
+
+        default:
+
+      }
+    })
+}
+
+const viewEmployees = async() => {
+  let [employeeRows, data] = await connection.query("SELECT * from employeesdb.employee");
+  console.table(employeeRows);
+  return;
+}
--- employeesdb.sql
+++ employeesdb.sql
@@ -23,5 +23,6 @@ id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR (30) NOT NULL,
 role_id INT NOT NULL,
+manager_id INT,
 PRIMARY KEY (id)
 );
