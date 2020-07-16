5ff2bd16c77a4f82fdff9e16286e7c7512f85c37
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-15 20:17:01 -0400

    Created start function that brings up first prompt.

--- app.js
+++ app.js
@@ -22,13 +22,24 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  console.log("Server is listening on port " + PORT);
+  start();
+
 });
+console.log("Server is listening on port " + PORT);
 
 connection.end();
 
 //Set up inquirer prompts.
 //First prompt should have options to add an employee, view an employee, or update employee role.
+function start() {
+  inquirer
+    .prompt({
+      name: "startOptions",
+      type: "list",
+      message: "Would you like to add, update, or view employees?",
+      choices: ["Add employee", "Update Employee", "View employees", "Exit"]
+    })
+  }
 //If add employee, should ask you first name, then last name, then list of roles, then id.
 //If update employee roles, list of roles to replace current role.
 //If view employees is chose, should show employees witht heir department, and roles.
